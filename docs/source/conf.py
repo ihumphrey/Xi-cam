ed9fa8afa87a4525c37889ce9ec9a2adcfa3aa1e
Ian Humphrey <ian.r.humphrey@gmail.com>
2019-08-02 17:55:05 -0700

    Remove unnecessary sys.path changes in sphinx conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,6 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../../'))
 from recommonmark.transform import AutoStructify
 from sphinx.ext import autodoc
 import sphinx_bootstrap_theme
